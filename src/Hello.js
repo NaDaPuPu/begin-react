3ce6535d273dccb8834252b719771e7ccb5665b7
NaDaPuPu <jun19998@gmail.com>
2021-03-09 17:45:23 +0900

    Chapter 1.5. Props Destructuring assignment

--- src/Hello.js
+++ src/Hello.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-function Hello(props) {
-    return <div style={{ color : props.color }}>안녕하세요! {props.name}</div> // 왜 중괄호 두 번 사용?
+function Hello({ color, name }) {
+    return <div style={{ color }}>안녕하세요! {name}</div>
 }
 
 export default Hello;

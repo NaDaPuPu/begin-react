ffde5ab317d343529e72ab56fb6e0ea01e0e3111
NaDaPuPu <jun19998@gmail.com>
2021-03-09 17:35:12 +0900

    Chapter 1.5. Props inline style

--- src/Hello.js
+++ src/Hello.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Hello(props) {
-    return <div>안녕하세요! {props.name}</div>
+    return <div style={{ color : props.color }}>안녕하세요! {props.name}</div> // 왜 중괄호 두 번 사용?
 }
 
 export default Hello;

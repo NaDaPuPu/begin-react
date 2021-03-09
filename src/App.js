db95e31c2cb0c4617e8c9dedb33a13682e6441d0
NaDaPuPu <jun19998@gmail.com>
2021-03-09 16:36:30 +0900

    Chapter 1.4. JSX - using fragment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
+import React from 'react';
 import Hello from './Hello';
 
 function App() {
   return (
-    <div>
+    // <> : React fragment (key가 없을 때) : 두 개 이상의 태그는 꼭 하나의 태그로 감싸져있어야 한다. -> div 대신 사용(불필요한 출력 방지)
+    <>
       <Hello />
-      <Hello />
-      <Hello />
-    </div>
+      <div>안녕히계세요</div>
+    </>
   );
 }
 

acd3e1ec14f197b398523a4bd9da56072ab66086
NaDaPuPu <jun19998@gmail.com>
2021-03-09 16:41:58 +0900

    Chapter 1.4. JSX - using JavaScript

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import Hello from './Hello';
 
 function App() {
+  const name = 'react';
   return (
     // <> : React fragment (key가 없을 때) : 두 개 이상의 태그는 꼭 하나의 태그로 감싸져있어야 한다. -> div 대신 사용(불필요한 출력 방지)
     <>
       <Hello />
-      <div>안녕히계세요</div>
+      <div>{name}</div>
     </>
   );
 }

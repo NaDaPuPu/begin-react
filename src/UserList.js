9d8dbe956a336ef9fc521368ab32ab51c6a25b75
NaDaPuPu <jun19998@gmail.com>
2021-03-10 16:05:31 +0900

    Chapter 1.11. Rendering with 'map()'

--- src/UserList.js
+++ src/UserList.js
@@ -28,9 +28,9 @@ function UserList() {
     ];
     return (
         <div>
-            <User user={users[0]} />
-            <User user={users[1]} />
-            <User user={users[2]} />
+            {(users.map((user, index) => (
+                <User user={user} key={user.id} />
+            )))}
         </div>
     );
 }

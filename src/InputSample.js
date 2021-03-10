d195b693b4c2421d7db2946f79c3222ae8220f6a
NaDaPuPu <jun19998@gmail.com>
2021-03-10 15:07:48 +0900

    Chapter 1.10. useRef

--- src/InputSample.js
+++ src/InputSample.js
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 function InputSample() {
     const [inputs, setInputs] = useState({ // useState에 객체 형식의 상태를 전달
         name: '',
         nickname: ''
     });
+
+    const nameInput = useRef();
 
     const { name, nickname } = inputs; // 비구조화 할당을 통한 값 추출
 
@@ -21,12 +23,24 @@ function InputSample() {
             name: '',
             nickname: ''
         });
+        nameInput.current.focus();
     };
 
     return (
         <div>
-            <input name="name" onChange={onChange} value={name} placeholder="이름" />
-            <input name="nickname" onChange={onChange} value={nickname} placeholder="닉네임" />
+            <input
+                name="name"
+                onChange={onChange} 
+                value={name} 
+                placeholder="이름" 
+                ref={nameInput}
+            />
+            <input 
+                name="nickname" 
+                onChange={onChange} 
+                value={nickname} 
+                placeholder="닉네임" 
+            />
             <button onClick={onReset}>초기화</button>
             <div>
                 <b>값 : </b>

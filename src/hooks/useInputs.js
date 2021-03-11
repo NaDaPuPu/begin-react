87b9ea26656f60fc0adcd647555cbc620fa0e044
NaDaPuPu <jun19998@gmail.com>
2021-03-12 03:26:11 +0900

    Chapter 1.21. Custom Hook - My Answer

--- src/hooks/useInputs.js
+++ src/hooks/useInputs.js
@@ -1,14 +1,48 @@
-import { useState, useCallback } from 'react';
+import { useReducer, useCallback } from 'react';
+// function useInputs(initialForm) {
+//     const [form, setForm] = useState(initialForm);
+//     // Change
+//     const onChange = useCallback(e => {
+//         const { name, value } = e.target;
+//         setForm(form => ({ ...form, [name]: value }));
+//     }, []);
+//     const reset = useCallback(() => setForm(initialForm), [initialForm]);
+//     return [form, onChange, reset];
+// }
+function reducer(state, action) {
+    switch(action.type) {
+        case 'CHANGE_INPUT':
+            return{
+                ...state,
+                [action.name]: action.value
+            };
+        case 'RESET_INPUT':
+            return action.form
+        default:
+            return state;
+    }
+}
 
 function useInputs(initialForm) {
-    const [form, setForm] = useState(initialForm);
-    // Change
+    const [state, dispatch] = useReducer(reducer, initialForm);
+
     const onChange = useCallback(e => {
         const { name, value } = e.target;
-        setForm(form => ({ ...form, [name]: value }));
+        dispatch({
+            type: 'CHANGE_INPUT',
+            name,
+            value
+        });
     }, []);
-    const reset = useCallback(() => setForm(initialForm), [initialForm]);
-    return [form, onChange, reset];
+
+    const reset = useCallback(() => {
+        dispatch({
+            type: 'RESET_INPUT',
+            form: initialForm
+        });
+    }, [initialForm]);
+
+    return [state, onChange, reset];
 }
 
 export default useInputs;

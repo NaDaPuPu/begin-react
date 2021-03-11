715c7fa2bae8cae787d307ab2f490d03a04bc04f
NaDaPuPu <jun19998@gmail.com>
2021-03-11 14:25:11 +0900

    Chapter 1.17. useMemo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useMemo } from 'react';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
+
+function countActiveUsers(users) {
+  console.log('활성 사용자 수를 세는 중...');
+  return users.filter(user => user.active).length;
+}
 
 function App() {
   const [inputs, setinputs] = useState({
@@ -71,6 +76,8 @@ function App() {
     );
   }
 
+  const count = useMemo(() => countActiveUsers(users), [users]);
+
   return (
     <>
       <CreateUser 
@@ -80,6 +87,7 @@ function App() {
         onCreate={onCreate}
       />
       <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
+      <div>활성 사용자 수 : {count}</div>
     </>
   );
 }

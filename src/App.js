202eeaaa57bc1169220f472728e2eb84de4483af
NaDaPuPu <jun19998@gmail.com>
2021-03-11 19:18:51 +0900

    Chapter 1.20. Delete All Logics

--- src/App.js
+++ src/App.js
@@ -7,23 +7,12 @@ function countActiveUsers(users) {
   return users.filter(user => user.active).length;
 }
 
-function App() {
-  const [inputs, setinputs] = useState({
+const initialState = {
+  inputs: {
     username: '',
     email: ''
-  });
-
-  const { username, email } = inputs;
-  
-  const onChange = useCallback(e => {
-    const { name, value } = e.target;
-    setinputs(inputs => ({
-      ...inputs,
-      [name]: value
-    }));
-  }, []);
-
-  const [users, setUsers] = useState([
+  },
+  users: [
     {
       id: 1,
       username: 'NaDaPuPu',
@@ -42,56 +31,15 @@ function App() {
       email: 'osj@example.com',
       active: false
     }
-  ]);
+  ]
+}
 
-  const nextId = useRef(4);
-  const onCreate = useCallback(() => {
-    const user = {
-      id: nextId.current,
-      username,
-      email
-    };
-    setUsers(users => users.concat(user));
-
-    setinputs({
-      username: '',
-      email: ''
-    });
-
-    nextId.current += 1;
-  }, [username, email]); // users props, username, email state 사용
-  
-
-  const onRemove = useCallback(
-    id => {
-      // user.id가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듦
-      // = user.id가 id인 것을 제거함
-      setUsers(users => users.filter(user => user.id !== id));
-    }, [] // users props 사용
-  ); 
-
-  const onToggle = useCallback(
-    id => {
-      setUsers(users =>
-        users.map(user =>
-          user.id === id ? { ...user, active: !user.active } : user // user.id가 id와 같을 경우, 해당 user의 active를 변경한 배열 호출. 아닐 경우, user 배열 호출
-        )
-      );
-    }, [] // users props 사용
-  ); 
-
-  const count = useMemo(() => countActiveUsers(users), [users]);
-
+function App() {
   return (
     <>
-      <CreateUser 
-        username={username}
-        email={email}
-        onChange={onChange}
-        onCreate={onCreate}
-      />
-      <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
-      <div>활성 사용자 수 : {count}</div>
+      <CreateUser />
+      <UserList users={[]}/>
+      <div>활성 사용자 수 : 0</div>
     </>
   );
 }

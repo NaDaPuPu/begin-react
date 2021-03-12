97d4ed3e142f07ca2eef11bf396c31138548fc48
NaDaPuPu <jun19998@gmail.com>
2021-03-12 15:03:42 +0900

    Chapter 1.23. Immer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useReducer, useMemo } from 'react';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
+import produce from 'immer';
 
 function countActiveUsers(users) {
   console.log('활성 사용자 수를 세는 중...');
@@ -37,21 +38,32 @@ const initialState = {
 function reducer(state, action) {
   switch (action.type) {
     case 'CREATE_USER':
-      return {
-        users: state.users.concat(action.user)
-      };
+      // return {
+      //   users: state.users.concat(action.user)
+      // };
+      return produce(state, draft => {
+        draft.users.push(action.user);
+      });
     case 'TOGGLE_USER':
-      return {
-        ...state,
-        users: state.users.map(user =>
-          user.id === action.id ? {...user, active: !user.active } : user
-        )
-      };
+      // return {
+      //   ...state,
+      //   users: state.users.map(user =>
+      //     user.id === action.id ? {...user, active: !user.active } : user
+      //   )
+      // };
+      return produce(state, draft => {
+        const user = draft.users.find(user => user.id === action.id); // find() : 조건에 맞는 요소 출력
+        user.active = !user.active;
+      });
     case 'REMOVE_USER':
-      return {
-        ...state,
-        users: state.users.filter(user => user.id !== action.id)
-      };
+      // return {
+      //   ...state,
+      //   users: state.users.filter(user => user.id !== action.id)
+      // };
+      return produce(state, draft => {
+        const index = draft.users.findIndex(user => user.id === action.id); // findIndex() : 조건에 맞는 요소의 인덱스 출력
+        draft.users.splice(index, 1); // 인덱스가 index인 요소를 1개 삭제
+      });
     default:
       return state;
   }

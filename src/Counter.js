62705136580a7a733bb22cfc46251917cb044cf3
NaDaPuPu <jun19998@gmail.com>
2021-03-12 16:12:15 +0900

    Chapter 1.24. If class-properties is available

--- src/Counter.js
+++ src/Counter.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            counter: 0
-        };
-    }
+    state = {
+        counter: 0
+    };
 
     handleIncrease = () => {
         console.log('Increase');
